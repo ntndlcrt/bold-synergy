2fcf681ae3a4186ac509c2d28bbc19cdeb0b0a90
ntndlcrt <adelcourte.pro@gmail.com>
2025-01-08 22:23:39 +0100

    Update HeroSlider links text

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
             text: 'Made in Saudi<br/>solutions for global<br/>excellence',
             link: {
               href: '/about-us/',
-              title: 'Learn more about us',
+              title: 'Learn more',
             },
             src: '/images/slider-home/1.png',
           },
@@ -22,7 +22,7 @@ export default function Home() {
             text: 'Future-proof your business and chart your transformation journey with us',
             link: {
               href: '/about-us/',
-              title: 'Learn more about us',
+              title: 'Learn more',
             },
             src: '/images/slider-home/2.png',
           },
@@ -31,7 +31,7 @@ export default function Home() {
             text: 'In the age of disruption, navigate the complexities of modern commerce with confidence ',
             link: {
               href: '/about-us/',
-              title: 'Learn more about us',
+              title: 'Learn more',
             },
             src: '/images/slider-home/3.png',
           },
@@ -40,7 +40,7 @@ export default function Home() {
             text: 'Beyond the funnel, build a modern approach to Sales & Marketing',
             link: {
               href: '/about-us/',
-              title: 'Learn more about us',
+              title: 'Learn more',
             },
             src: '/images/slider-home/4.png',
           },
@@ -49,7 +49,7 @@ export default function Home() {
             text: 'Beyond the buzz, strategic insights for a connected world',
             link: {
               href: '/about-us/',
-              title: 'Learn more about us',
+              title: 'Learn more',
             },
             src: '/images/slider-home/5.png',
           },
@@ -61,7 +61,7 @@ export default function Home() {
         text="With a deep-rooted understanding of the Kingdom's unique market dynamics, regulatory environment, and cultural nuances, we empower Saudi firms to achieve sustainable growth and success. Our team of seasoned professionals boasts a proven track record of delivering tailored solutions that address the specific challenges and opportunities faced by Saudi businesses. We combine our in-depth industry expertise with a client-centric approach to forge lasting partnerships that drive tangible results."
         link={{
           href: '/about-us/',
-          title: 'Learn more about us',
+          title: 'Learn more',
         }}
         subtitle="What sets Synergy Consulting apart:"
         items={[
